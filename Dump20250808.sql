034358999cdfca44e7885170ffcf9d1572d3fe2e
SnehaanKandaswamy <146335670+SnehaanKandaswamy@users.noreply.github.com>
2025-08-08 09:33:05 +0530

    Updated queries

--- Dump20250808.sql
+++ Dump20250808.sql
@@ -1,27 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
---
--- Host: localhost    Database: hospitaldbms
--- ------------------------------------------------------
--- Server version	8.0.42
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
 -- Table structure for table `appointment`
---
-
 DROP TABLE IF EXISTS `appointment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `appointment` (
   `appointment_id` int NOT NULL,
   `appointment_date` date NOT NULL,
@@ -30,16 +8,10 @@ CREATE TABLE `appointment` (
   `patient_id` int NOT NULL,
   `status` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`appointment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
 -- Table structure for table `bill`
---
-
 DROP TABLE IF EXISTS `bill`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bill` (
   `BILL_ID` int NOT NULL,
   `PATIENT_ID` int NOT NULL,
@@ -47,16 +19,12 @@ CREATE TABLE `bill` (
   `PAYMENT_STATUS` varchar(10) DEFAULT NULL,
   `BILL_DATE` date DEFAULT NULL,
   PRIMARY KEY (`BILL_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
 -- Table structure for table `patient`
---
-
+-- Table structure for table `patient`
+-- Create patient table first with proper primary key
 DROP TABLE IF EXISTS `patient`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `patient` (
   `NAME` varchar(20) DEFAULT NULL,
   `AGE` int NOT NULL,
@@ -65,16 +33,11 @@ CREATE TABLE `patient` (
   `ADDRESS` varchar(100) DEFAULT NULL,
   `PATIENT_ID` int NOT NULL,
   PRIMARY KEY (`PATIENT_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ;
 
---
--- Table structure for table `test_result`
---
-
+-- Create test_result table that references patient table
 DROP TABLE IF EXISTS `test_result`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `test_result` (
   `TEST_DATE` date DEFAULT NULL,
   `PATIENT_ID` int DEFAULT NULL,
@@ -83,16 +46,4 @@ CREATE TABLE `test_result` (
   PRIMARY KEY (`TEST_ID`),
   KEY `PATIENT_ID` (`PATIENT_ID`),
   CONSTRAINT `test_result_ibfk_1` FOREIGN KEY (`PATIENT_ID`) REFERENCES `patient` (`PATIENT_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-08-08  9:18:50
+) ;
